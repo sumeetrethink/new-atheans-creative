967f062f790be930d8c26caec2d91c25a1f38f87
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-05-08 16:35:42 +0530

    admin can buessin approve

--- resources/views/Admin/Business/table.blade.php
+++ resources/views/Admin/Business/table.blade.php
@@ -1,4 +1,4 @@
-@foreach ($Businesses as $item)
+@foreach ($Businesses as $index => $item)
     <tr>
         <td>{{ $loop->iteration }}</td>
         <td>{{ $item->email}}</td>
@@ -6,6 +6,9 @@
         <td>{{ $item->address }}</td>
 
         <td>
+            <button type="button" title="Click here to changes status"
+            onclick="chanStatus({{ $item->id}},{{$index }})"
+            class="btn {{ $item->is_approved == 'Yes' ? 'btn-success' : 'btn-danger' }} admin-business-status-{{ $index }}">{{ $item->is_approved == 'Yes' ? 'Active' : 'InActive' }}</button>
             <a title="Click here to Edit" href="{{ url('admin/business/view/?item=' . $item->id) }}" class="btn btn-primary"
                 style="margin-right:5px"><i class="fa fa-pen"></i></a>
             <button type="button" title="Click here to Delete" onclick="openModal({{ $item->id }})"
@@ -17,5 +20,22 @@
     function openModal(id) {
         $("#modal-default").modal("show");
         $("#deleteId").val(id);
+    }
+    function chanStatus(id,index) {
+        $.ajax({
+            url: BASE_URL + "/admin/business/change-status?id=" + id,
+            success: function(data) {
+
+                if (data == "Yes") {
+                    $(`.admin-business-status-${index}`).removeClass().addClass(
+                        `btn btn-success admin-business-status-${index}`);
+                    $(`.admin-business-status-${index}`).html("Active")
+                } else {
+                    $(`.admin-business-status-${index}`).removeClass().addClass(
+                        `btn btn-danger admin-business-status-${index}`);
+                    $(`.admin-business-status-${index}`).html("InActive")
+                }
+            }
+        })
     }
 </script>
